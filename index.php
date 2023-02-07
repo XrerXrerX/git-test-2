f0f71a22ff9d935b51b9f1e785587b1accce1583
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-02-07 21:33:22 +0700

    menambah list

--- index.php
+++ index.php
@@ -10,5 +10,6 @@
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sequi quam amet suscipit alias minima, inventore dolorum modi veritatis repellendus! Earum consequatur voluptatibus rerum eos beatae laboriosam nulla cupiditate possimus?</p>
 <h1>halo</h1>
+<h3>halooo</h3>
 </body>
 </html>
