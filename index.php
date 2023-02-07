7aaf9d4b10a98645e0d1e2ae23b4fc5ce4897928
XrerXrerX <50944603+XrerXrerX@users.noreply.github.com>
2023-02-07 21:16:06 +0700

    menambah h1

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 </head>
 <body>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sequi quam amet suscipit alias minima, inventore dolorum modi veritatis repellendus! Earum consequatur voluptatibus rerum eos beatae laboriosam nulla cupiditate possimus?</p>
+<h1>halo</h1>
 </body>
 </html>
